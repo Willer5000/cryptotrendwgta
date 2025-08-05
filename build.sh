269f5d6e5cb6a5125a40cb9071703e5c3412761f
Willer Torrico <109767216+Willer5000@users.noreply.github.com>
2025-08-05 01:26:29 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-echo "No se requieren acciones de compilación especiales"
+set -e
+
+# Instalar dependencias del sistema
+sudo apt-get update
+sudo apt-get install -y build-essential
+
+# Descargar y compilar TA-Lib
+wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
+tar -xzf ta-lib-0.4.0-src.tar.gz
+cd ta-lib/
+./configure --prefix=/usr
+make
+sudo make install
+cd ..
+
+# Instalar dependencias de Python
+pip install -r requirements.txt
