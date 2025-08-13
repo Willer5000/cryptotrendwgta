ab3c3610a6ea6ba4b4beead7b73b5d6402ca07a8
Willer Torrico <109767216+Willer5000@users.noreply.github.com>
2025-08-13 01:30:59 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-echo ">>> Actualizando pip y setuptools"
-python -m pip install --upgrade pip setuptools wheel
-
-echo ">>> Instalando dependencias"
-pip install -r requirements.txt --no-cache-dir
-
-echo ">>> Instalación completada"
+echo "Instalando dependencias..."
+pip install -r requirements.txt
