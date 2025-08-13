52e6a5afdda34c2678b08d85f71dbd759ace3b15
Willer Torrico <109767216+Willer5000@users.noreply.github.com>
2025-08-13 20:41:01 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,4 @@ echo ">>> Actualizando pip"
 python -m pip install --upgrade pip
 
 echo ">>> Instalando dependencias"
-pip install -r requirements.txt --no-cache-dir
-
-echo ">>> Instalación completada"
+pip install -r requirements.txt
