6eadd123976de9fe38a050d6820a575547832027
Willer Torrico <109767216+Willer5000@users.noreply.github.com>
2025-08-14 18:01:01 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 echo "Instalando dependencias..."
 pip install -r requirements.txt
+
+# Instalar TA-Lib
+echo "Instalando TA-Lib..."
+sudo apt-get update
+sudo apt-get install -y build-essential libta-lib-dev
+pip install TA-Lib
